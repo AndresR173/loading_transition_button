fc50364d3c6988aad2604f50249e672cdca94b39
Andres Rojas <afro17@gmail.com>
2021-05-16 12:43:17 -0500

    Update animations

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,68 +6,78 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
   final _controller = LoadingButtonController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Builder(
-      builder: (BuildContext context) {
-        return Scaffold(
-          backgroundColor: Colors.white,
-          body: Container(
-            width: double.infinity,
-            color: Colors.black26,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 44,
-                  width: 300,
-                  child: LoadingButton(
-                    color: Colors.blue,
-                    onSubmit: () => print('onSubmit'),
-                    controller: _controller,
-                    errorColor: Colors.red,
-                    transitionDuration: Duration(seconds: 1),
-                    child: Text(
-                      'Hit me!',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText1
-                          .copyWith(color: Colors.white),
+    return MaterialApp(
+      home: Builder(
+        builder: (BuildContext context) {
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: Container(
+              width: double.infinity,
+              color: Colors.black26,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 44,
+                    width: 300,
+                    child: LoadingButton(
+                      color: Colors.blue,
+                      onSubmit: () => print('onSubmit'),
+                      controller: _controller,
+                      errorColor: Colors.red,
+                      transitionDuration: Duration(seconds: 1),
+                      child: Text(
+                        'Hit me!',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .copyWith(color: Colors.white),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 100),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    RaisedButton(
-                      onPressed: () => _controller.startLoadingAnimation(),
-                      child: Text('Start'),
-                    ),
-                    RaisedButton(
-                      onPressed: () => _controller.stopLoadingAnimation(),
-                      child: Text('Stop'),
-                    ),
-                    RaisedButton(
-                      onPressed: () => _controller.onError(),
-                      child: Text('Error'),
-                    ),
-                    RaisedButton(
-                      onPressed: () => _controller.moveToScreen(
-                          context: context, page: SearchPage()),
-                      child: Text('Next screen'),
-                    )
-                  ],
-                )
-              ],
+                  SizedBox(height: 100),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => _controller.startLoadingAnimation(),
+                        child: Text('Start'),
+                      ),
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: () => _controller.stopLoadingAnimation(),
+                        child: Text('Stop'),
+                      ),
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: () => _controller.onError(),
+                        child: Text('Error'),
+                      ),
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: () => _controller.moveToScreen(
+                          context: context,
+                          page: SearchPage(),
+                          stopAnimation: true,
+                          navigationCallback: (route) =>
+                              Navigator.of(context).push(route),
+                        ),
+                        child: Text('Next screen'),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        );
-      },
-    ));
+          );
+        },
+      ),
+    );
   }
 }
 
